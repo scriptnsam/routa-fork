81e217890be9fb8ecc46fde91f3b03b1fb892163
scriptnsam <oluwafemisam40@gmail.com>
2025-12-19 12:43:18 +0100

    fix: Include Migrate script

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({ adapter });
